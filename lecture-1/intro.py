4fbf016f6689c7c48fe0f28b5b42bb717c5f5a0d
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2023-10-03 00:24:32 +0300

    Intro

---
+++ lecture-1/intro.py
@@ -0,0 +1,26 @@
+#Hello World
+
+print ('Hello World')
+
+# Case Sensitive
+average =10
+
+total_students=20
+
+def printMyName(name):
+    print("Hello" +" "+ name)
+
+
+printMyName('Ahmed')
+
+#By Default Every command is on one line
+x=1
+z=3
+y=5
+
+#You can combine them by adding ; between them
+x=9; z=10; y=9
+
+
+
+
